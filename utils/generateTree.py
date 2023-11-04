8ceaf919efb49142919f3f33a12acb110dc5aa94
Jaithecomder <jaib2910@gmail.com>
2023-11-04 06:00:23 +0530

    tree generation with SC (not tested)

--- utils/generateTree.py
+++ utils/generateTree.py
@@ -43,7 +43,7 @@ def generateNodes( limit : int = 1, specific : int = -1, unwanted : list = [-1] 
     return nodes
 
 
-def generateInitialTreeDepth2( treeSize : int = 5 ):
+def generateInitialTreeDepth2( treeSize : int = 5, SC : bool = False ):
     '''
         Creates a tree with selector node as root node
         and its children as some number of condition nodes
@@ -53,7 +53,17 @@ def generateInitialTreeDepth2( treeSize : int = 5 ):
     root = SelectorNode()
     
     numberOfNodes = random.randint(2, treeSize - 1)
-    children = generateNodes( limit = numberOfNodes, unwanted = [2,3] )
+    children = []
+    if SC:
+        if numberOfNodes == 1:
+            children = generateNodes( limit = numberOfNodes, specific = 0 )
+        else:
+            numConditions = random.randint(1, numberOfNodes - 1)
+            conditionChildren = generateNodes( limit = numConditions, unwanted = [2,3] )
+            actionChildren = generateNodes( limit = numberOfNodes - numConditions, specific = 0 )
+            children = conditionChildren + actionChildren
+    else:
+        children = generateNodes( limit = numberOfNodes, unwanted = [2,3] )
 
     for child in children:
         root.addChild( child )
@@ -62,7 +72,7 @@ def generateInitialTreeDepth2( treeSize : int = 5 ):
     generatedTree.updateExecutionOrder()
     return generatedTree
     
-def generateInitialTreeDepth3( treeSize : int = 7 ):
+def generateInitialTreeDepth3( treeSize : int = 7, SC : bool = False ):
     '''
         Creates a tree with selector node as root node
         and its children as a random list of recursicve children.
@@ -71,45 +81,68 @@ def generateInitialTreeDepth3( treeSize : int = 7 ):
     
     remaining = treeSize
 
-    numberOfSequenceNodes = random.randint(1, floor(2*treeSize/5) - 1)
-    children = generateNodes( limit = numberOfSequenceNodes , specific = 3 )
+    numberOfControllerNodes = random.randint(1, floor(2*treeSize/5) - 1)
+    if SC:
+        children = generateNodes( limit = numberOfControllerNodes , specific = 3 )
+    else:
+        children = generateNodes( limit = numberOfControllerNodes , unwanted = [0,1] )
 
-    remaining -= numberOfSequenceNodes
-    children += generateNodes( limit = treeSize - 2*numberOfSequenceNodes, unwanted = [2,3] )
+    remaining -= numberOfControllerNodes
+    if SC:
+        numConditions = random.randint(1, treeSize - 2*numberOfControllerNodes - 1)
+        children += generateNodes( limit = numConditions, specific=1 )
+        children += generateNodes( limit = treeSize - 2*numberOfControllerNodes - numConditions, specific=0 )
+    else:
+        children += generateNodes( limit = treeSize - 2*numberOfControllerNodes, unwanted = [2,3] )
     
-    remaining -= treeSize - 2*numberOfSequenceNodes
+    remaining -= treeSize - 2*numberOfControllerNodes
 
     for child in children:
-        if ( isinstance(child, SelectorNode) or isinstance(child, SequenceNode) and remaining > 0 ):
+        if ( isinstance(child, SelectorNode) or isinstance(child, SequenceNode) and remaining > numberOfControllerNodes ):
             numGrandChildren = 0
-            if ( remaining > 1 ):
-                numGrandChildren = random.randint(1, remaining) 
+            if ( remaining > numberOfControllerNodes + 1 ):
+                numGrandChildren = random.randint(1, remaining - numberOfControllerNodes) 
             else :
                 numGrandChildren = 1
             
             grandChildren = []
-            if ( numGrandChildren > 1 ):
+            if SC:
+                if numGrandChildren == 1:
+                    grandChildren = generateNodes( limit = numGrandChildren, specific=0 )
+                else:
+                    numConditions = random.randint(1, numGrandChildren - 1)
+                    grandChildren += generateNodes( limit = numConditions, specific=1 )
+                    grandChildren += generateNodes( limit = numGrandChildren - numConditions, specific=0 )
+            else:
+                # if ( numGrandChildren > 1 ):
                 grandChildren += generateNodes( limit = numGrandChildren - 1 , unwanted = [2,3] )
-            
-            grandChildren += generateNodes( specific = 0 )
+                
+                # grandChildren += generateNodes( specific = 0 )
 
             for grandChild in grandChildren:
                 child.addChild( grandChild )
             remaining -= numGrandChildren
+        elif ( isinstance(child, SelectorNode) or isinstance(child, SequenceNode) and remaining == numberOfControllerNodes ):
+            if SC:
+                child.addChild( generateNodes( specific = 0 )[0] )
+            else:
+                child.addChild( generateNodes( unwanted = [2,3] )[0] )
+            remaining -= 1
         root.addChild( child )
+        numberOfControllerNodes -= 1
 
     generatedTree = Tree( root )
     generatedTree.updateExecutionOrder()
     return generatedTree
 
-def generateInitialTrees( numTrees : int = 2, depth2SizeLimit : int = 5, depth3SizeLimit : int = 7 ) -> list :
+def generateInitialTrees( numTrees : int = 2, depth2SizeLimit : int = 5, depth3SizeLimit : int = 7, SC : bool = False ) -> list :
     trees = []
     for _ in range( numTrees ):
         toss = random.randint(0, 1)
         if toss == 0:
-            trees.append( generateInitialTreeDepth2(depth2SizeLimit) )
+            trees.append( generateInitialTreeDepth2(depth2SizeLimit, SC) )
         else:
-            trees.append( generateInitialTreeDepth3(depth3SizeLimit) )
+            trees.append( generateInitialTreeDepth3(depth3SizeLimit, SC) )
 
     return trees
 
